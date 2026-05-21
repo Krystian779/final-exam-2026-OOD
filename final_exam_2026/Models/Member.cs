01491234a3d304aa07406cf518cec5319a1bb0d8
Krystian779 <krischmielak@gmail.com>
2026-05-21 13:08:10 +0100

    Created Classes

---
+++ final_exam_2026/Models/Member.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final_exam_2026.Models
+{
+    public class Member
+    {
+
+        // Properties
+        public int MemberId { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ContactNumber { get; set; }
+        public string MembershipType { get; set; }
+
+        // Constructors
+
+        // Methods
+
+    }
+}
